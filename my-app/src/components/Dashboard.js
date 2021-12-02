0b31d1579a18fcd2c8cbf7a60a6b675830f09c41
Manideepakreddi <manideepreddi007@gmail.com>
2021-12-02 11:07:08 -0500

    projectheader

--- my-app/src/components/Dashboard.js
+++ my-app/src/components/Dashboard.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
+import ProjectItem from "./project/ProjectItem";
 
 class Dashboard extends Component {
     render() {
-            return <h1>Welcome to Dashboard</h1>;
+        return (
+          <div>
+            <h1>Welcome to the Dashboard</h1>
+            <ProjectItem />
+            </div>
+        );
+      }
         
-    }
+    
 }
 export default Dashboard;
