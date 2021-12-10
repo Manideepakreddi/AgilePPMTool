34546023290975a0dc27fc1855d864fc83ab53fa
Manideepakreddi <manideepreddi007@gmail.com>
2021-12-10 15:21:21 -0500

    delete ProjectTask & Project

--- AgileIntPPMTool/PPMToolFullStack/src/main/java/hcl/domain/Backlog.java
+++ AgileIntPPMTool/PPMToolFullStack/src/main/java/hcl/domain/Backlog.java
@@ -38,7 +38,7 @@ public class Backlog {
     private Project project;
 
     //OneToMany projecttasks
-    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "backlog")
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH, mappedBy = "backlog", orphanRemoval = true)
     private List<ProjectTask> projectTasks = new ArrayList<>();
 
 
