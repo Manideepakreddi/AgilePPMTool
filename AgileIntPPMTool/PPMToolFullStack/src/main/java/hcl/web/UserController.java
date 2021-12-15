9faace03b05a918996f11bb5ab8ba9ce85ce0178
Manideepakreddi <manideepreddi007@gmail.com>
2021-12-15 13:53:43 -0500

    validation password

--- AgileIntPPMTool/PPMToolFullStack/src/main/java/hcl/web/UserController.java
+++ AgileIntPPMTool/PPMToolFullStack/src/main/java/hcl/web/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import hcl.Validator.UserValidator;
 import hcl.domain.User;
 import hcl.services.MapValidationErrorService;
 import hcl.services.UserService;
@@ -25,10 +26,14 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    
+    @Autowired
+    private UserValidator userValidator;
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result){
         // Validate passwords match
+    	userValidator.validate(user, result);
 
         ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
         if(errorMap != null)return errorMap;
