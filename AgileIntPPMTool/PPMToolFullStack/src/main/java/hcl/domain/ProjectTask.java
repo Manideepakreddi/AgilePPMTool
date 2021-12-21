089114db49c9330c82b36efdf2bbbb2b1d3a8c92
Manideepakreddi <manideepreddi007@gmail.com>
2021-12-21 11:16:50 -0500

    bug fix

--- AgileIntPPMTool/PPMToolFullStack/src/main/java/hcl/domain/ProjectTask.java
+++ AgileIntPPMTool/PPMToolFullStack/src/main/java/hcl/domain/ProjectTask.java
@@ -3,6 +3,7 @@ package hcl.domain;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
@@ -17,25 +18,28 @@ import java.util.Date;
 @Data
 public class ProjectTask {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(updatable = false, unique = true)
-    private String projectSequence;
-    @NotBlank(message = "Please include a project summary")
-    private String summary;
-    private String acceptanceCriteria;
-    private String status;
-    private Integer priority;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(updatable = false, unique = true)
+	private String projectSequence;
+	@NotBlank(message = "Please include a project summary")
+	private String summary;
+	private String acceptanceCriteria;
+	private String status;
+	private Integer priority;
+	@JsonFormat(pattern = "yyyy-mm-dd")
     private Date dueDate;
-    //ManyToOne with Backlog
-    @ManyToOne(fetch = FetchType.EAGER) 
-    @JoinColumn(name = "backlog_id", updatable = false, nullable = false)
-    @JsonIgnore
-    private Backlog backlog;
-    @Column(updatable = false)
-    private String projectIdentifier;
+	// ManyToOne with Backlog
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "backlog_id", updatable = false, nullable = false)
+	@JsonIgnore
+	private Backlog backlog;
+	@Column(updatable = false)
+	private String projectIdentifier;
+	@JsonFormat(pattern = "yyyy-mm-dd")
     private Date create_At;
+	@JsonFormat(pattern = "yyyy-mm-dd")
     private Date update_At;
 
 	/*
@@ -84,29 +88,21 @@ public class ProjectTask {
 	 * 
 	 * public void setUpdate_At(Date update_At) { this.update_At = update_At; }
 	 */
-    @PrePersist
-    protected void onCreate(){
-        this.create_At = new Date();
-    }
+	@PrePersist
+	protected void onCreate() {
+		this.create_At = new Date();
+	}
 
-    @PreUpdate
-    protected void onUpdate(){
-        this.update_At = new Date();
-    }
+	@PreUpdate
+	protected void onUpdate() {
+		this.update_At = new Date();
+	}
 
-    @Override
-    public String toString() {
-        return "ProjectTask{" +
-                "id=" + id +
-                ", projectSequence='" + projectSequence + '\'' +
-                ", summary='" + summary + '\'' +
-                ", acceptanceCriteria='" + acceptanceCriteria + '\'' +
-                ", status='" + status + '\'' +
-                ", priority=" + priority +
-                ", dueDate=" + dueDate +
-                ", projectIdentifer='" + projectIdentifier + '\'' +
-                ", create_At=" + create_At +
-                ", update_At=" + update_At +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "ProjectTask{" + "id=" + id + ", projectSequence='" + projectSequence + '\'' + ", summary='" + summary
+				+ '\'' + ", acceptanceCriteria='" + acceptanceCriteria + '\'' + ", status='" + status + '\''
+				+ ", priority=" + priority + ", dueDate=" + dueDate + ", projectIdentifer='" + projectIdentifier + '\''
+				+ ", create_At=" + create_At + ", update_At=" + update_At + '}';
+	}
 }
