3fbc61b435e9151ea5485ea3634ab416b180f400
Manideepakreddi <manideepreddi007@gmail.com>
2021-12-21 10:18:20 -0500

    locking public routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,9 +3,9 @@ import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AddProject from "./components/project/AddProject";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 import store from "./store";
 import UpdateProject from "./components/project/UpdateProject";
 import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
@@ -18,9 +18,10 @@ import jwt_decode from "jwt-decode";
 import setJWTToken from "./securityUtils/setJWTToken";
 import { SET_CURRENT_USER } from "./actions/types";
 import { logout } from "./actions/securityActions";
+import SecuredRoute from "./securityUtils/SecureRoute";
 
+const jwtToken = localStorage.jwtToken;
 
-const jwtToken=localStorage.jwtToken
 if (jwtToken) {
   setJWTToken(jwtToken);
   const decoded_jwtToken = jwt_decode(jwtToken);
@@ -31,11 +32,10 @@ if (jwtToken) {
 
   const currentTime = Date.now() / 1000;
   if (decoded_jwtToken.exp < currentTime) {
-      store.dispatch(logout());
-      window.location.href = "/";
+    store.dispatch(logout());
+    window.location.href = "/";
   }
 }
-
 
 class App extends Component {
   render() {
@@ -45,30 +45,40 @@ class App extends Component {
           <div className="App">
             <Header />
             {
-
+              //Public Routes
             }
 
             <Route exact path="/" component={Landing} />
-            <Route exact path="/register" component={Register}/>
-            <Route exact path="/login" component={Login}/>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
 
             {
-
+              //Private Routes
             }
-            <Route exact path="/dashboard" component={Dashboard} />
-            <Route exact path="/addProject" component={AddProject} />
-            <Route exact path="/updateProject/:id" component={UpdateProject} />
-            <Route exact path="/projectBoard/:id" component={ProjectBoard} />
-            <Route
-              exact
-              path="/addProjectTask/:id"
-              component={AddProjectTask}
-            />
-            <Route
-              exact
-              path="/updateProjectTask/:backlog_id/:pt_id"
-              component={UpdateProjectTask}
-            />
+            <Switch>
+              <SecuredRoute exact path="/dashboard" component={Dashboard} />
+              <SecuredRoute exact path="/addProject" component={AddProject} />
+              <SecuredRoute
+                exact
+                path="/updateProject/:id"
+                component={UpdateProject}
+              />
+              <SecuredRoute
+                exact
+                path="/projectBoard/:id"
+                component={ProjectBoard}
+              />
+              <SecuredRoute
+                exact
+                path="/addProjectTask/:id"
+                component={AddProjectTask}
+              />
+              <SecuredRoute
+                exact
+                path="/updateProjectTask/:backlog_id/:pt_id"
+                component={UpdateProjectTask}
+              />
+            </Switch>
           </div>
         </Router>
       </Provider>
